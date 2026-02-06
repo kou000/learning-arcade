c8dada170e6f69d62479da678e7b00266df60b7a
kou000 <daita1.siam@gmail.com>
2026-02-06 22:13:57 +0900

    chore: baseを修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pages のリポジトリ名が learning-arcade なので base を固定
 export default defineConfig({
   plugins: [react()],
-  base: "/learning-arcade/",
+  base: "/",
 });
